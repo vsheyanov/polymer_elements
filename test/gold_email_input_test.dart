71437c94a01c6cd8293f78fd2973bc705c5ff679
Victor Sheyanov <victor.sheyanov@gmail.com>
2015-08-11 14:27:55 +0300

    fixed styling with dartfmt tool

--- test/gold_email_input_test.dart
+++ test/gold_email_input_test.dart
@@ -7,136 +7,141 @@ import 'package:test/test.dart';
 import 'common.dart';
 
 main() async {
-    await initWebComponents();
+  await initWebComponents();
 
-    group('basic', () {
-        test('invalid input shows error', () {
-            GoldEmailInput input = fixture('basic');
-            input.value='1234';
-            input.jsElement.callMethod('_onInput');
-            forceXIfStamp(input);
+  group('basic', () {
+    test('invalid input shows error', () {
+      GoldEmailInput input = fixture('basic');
+      input.value = '1234';
+      input.jsElement.callMethod('_onInput');
+      forceXIfStamp(input);
 
-            var container = input.querySelector('paper-input-container');
-            expect(container, isNotNull);
-            expect(container.invalid, isTrue);
+      var container = input.querySelector('paper-input-container');
+      expect(container, isNotNull);
+      expect(container.invalid, isTrue);
 
-            var error = input.querySelector('paper-input-error');
-            expect(error, isNotNull);
-            expect(error.getComputedStyle().display, isNot('none'), reason: 'error is not display:none');
-        });
-
-        test('valid input does not show error', () {
-            GoldEmailInput input = fixture('basic');
-            input.value='batman@gotham.org';
-            input.jsElement.callMethod('_onInput');
-            forceXIfStamp(input);
-
-            var container = input.querySelector('paper-input-container');
-            expect(container, isNotNull);
-            expect(container.invalid, isFalse);
-
-            var error = input.querySelector('paper-input-error');
-            expect(error, isNotNull);
-            expect(error.getComputedStyle().display, equals('none'), reason: 'error should be display:none');
-        });
-
-        test('empty required input shows error', () {
-            GoldEmailInput input = fixture('basic');
-            forceXIfStamp(input);
-
-            var error = input.querySelector('paper-input-error');
-            expect(error, isNotNull);
-            expect(error.getComputedStyle().display, isNot('none'), reason: 'error is not display:none');
-        });
+      var error = input.querySelector('paper-input-error');
+      expect(error, isNotNull);
+      expect(error.getComputedStyle().display, isNot('none'),
+          reason: 'error is not display:none');
     });
 
-    group('a11y', () {
-        test('has aria-labelledby', () {
-            GoldEmailInput input = fixture('basic');
-            expect(input.inputElement.getAttribute('aria-labelledby'), isNotNull);
-            expect(input.inputElement.getAttribute('aria-labelledby'), input.querySelector('label').id, reason: 'aria-labelledby points to the label');
-        });
+    test('valid input does not show error', () {
+      GoldEmailInput input = fixture('basic');
+      input.value = 'batman@gotham.org';
+      input.jsElement.callMethod('_onInput');
+      forceXIfStamp(input);
+
+      var container = input.querySelector('paper-input-container');
+      expect(container, isNotNull);
+      expect(container.invalid, isFalse);
+
+      var error = input.querySelector('paper-input-error');
+      expect(error, isNotNull);
+      expect(error.getComputedStyle().display, equals('none'),
+          reason: 'error should be display:none');
     });
 
-    void testEmail(address, valid) {
-        GoldEmailInput input = fixture('basic');
-        forceXIfStamp(input);
+    test('empty required input shows error', () {
+      GoldEmailInput input = fixture('basic');
+      forceXIfStamp(input);
 
-        var container = input.querySelector('paper-input-container');
-        expect(container, isNotNull);
-
-        input.value = address;
-        input.jsElement.callMethod('_onInput');
-        var errorString = address + ' should be ' + (valid ? 'valid' : 'invalid');
-        expect(container.invalid, equals(!valid), reason: errorString);
-    }
-
-    group('valid email address validation', () {
-        test('valid email', () {
-            testEmail('email@domain.com', true);
-        });
-        test('email with a dot in the address field', () {
-            testEmail('firstname.lastname@domain.com', true);
-        });
-        test('email with a subdomain', () {
-            testEmail('email@subdomain.domain.com', true);
-        });
-        test('weird tlds', () {
-            testEmail('testing+contact@subdomain.domain.pizza', true);
-        });
-        test('plus sign is ok', () {
-            testEmail('firstname+lastname@domain.com', true);
-        });
-        test('domain is valid ip', () {
-            testEmail('email@123.123.123.123', true);
-        });
-        test('digits in address', () {
-            testEmail('1234567890@domain.com', true);
-        });
-        test('dash in domain name', () {
-            testEmail('email@domain-one.com', true);
-        });
-        test('dash in address field', () {
-            testEmail('firstname-lastname@domain.com', true);
-        });
-        test('underscore in address field', () {
-            testEmail('_______@domain-one.com', true);
-        });
-        test('dot in tld', () {
-            testEmail('email@domain.co.jp', true);
-        });
+      var error = input.querySelector('paper-input-error');
+      expect(error, isNotNull);
+      expect(error.getComputedStyle().display, isNot('none'),
+          reason: 'error is not display:none');
     });
+  });
 
-    group('invalid email address validation', () {
-        test('missing @ and domain', () {
-            testEmail('plainaddress', false);
-        });
-        test('missing @', () {
-            testEmail('email.domain.com', false);
-        });
-        test('garbage', () {
-            testEmail('#@%^%#\$@#\$@#.com', false);
-        });
-        test('missing username', () {
-            testEmail('@domain.com', false);
-        });
-        test('has spaces', () {
-            testEmail('firstname lastname@domain.com', false);
-        });
-        test('encoded html', () {
-            testEmail('Joe Smith <email@domain.com>', false);
-        });
-        test('two @ signs', () {
-            testEmail('email@domain@domain.com', false);
-        });
-        test('unicode in address', () {
-            testEmail('?????@domain.com', false);
-        });
-        test('text after address', () {
-            testEmail('email@domain.com (Joe Smith)', false);
-        });
-        test('multiple dots in domain', () {
-            testEmail('email@domain..com', false);
-        });
+  group('a11y', () {
+    test('has aria-labelledby', () {
+      GoldEmailInput input = fixture('basic');
+      expect(input.inputElement.getAttribute('aria-labelledby'), isNotNull);
+      expect(input.inputElement.getAttribute('aria-labelledby'),
+          input.querySelector('label').id,
+          reason: 'aria-labelledby points to the label');
     });
+  });
+
+  void testEmail(address, valid) {
+    GoldEmailInput input = fixture('basic');
+    forceXIfStamp(input);
+
+    var container = input.querySelector('paper-input-container');
+    expect(container, isNotNull);
+
+    input.value = address;
+    input.jsElement.callMethod('_onInput');
+    var errorString = address + ' should be ' + (valid ? 'valid' : 'invalid');
+    expect(container.invalid, equals(!valid), reason: errorString);
+  }
+
+  group('valid email address validation', () {
+    test('valid email', () {
+      testEmail('email@domain.com', true);
+    });
+    test('email with a dot in the address field', () {
+      testEmail('firstname.lastname@domain.com', true);
+    });
+    test('email with a subdomain', () {
+      testEmail('email@subdomain.domain.com', true);
+    });
+    test('weird tlds', () {
+      testEmail('testing+contact@subdomain.domain.pizza', true);
+    });
+    test('plus sign is ok', () {
+      testEmail('firstname+lastname@domain.com', true);
+    });
+    test('domain is valid ip', () {
+      testEmail('email@123.123.123.123', true);
+    });
+    test('digits in address', () {
+      testEmail('1234567890@domain.com', true);
+    });
+    test('dash in domain name', () {
+      testEmail('email@domain-one.com', true);
+    });
+    test('dash in address field', () {
+      testEmail('firstname-lastname@domain.com', true);
+    });
+    test('underscore in address field', () {
+      testEmail('_______@domain-one.com', true);
+    });
+    test('dot in tld', () {
+      testEmail('email@domain.co.jp', true);
+    });
+  });
+
+  group('invalid email address validation', () {
+    test('missing @ and domain', () {
+      testEmail('plainaddress', false);
+    });
+    test('missing @', () {
+      testEmail('email.domain.com', false);
+    });
+    test('garbage', () {
+      testEmail('#@%^%#\$@#\$@#.com', false);
+    });
+    test('missing username', () {
+      testEmail('@domain.com', false);
+    });
+    test('has spaces', () {
+      testEmail('firstname lastname@domain.com', false);
+    });
+    test('encoded html', () {
+      testEmail('Joe Smith <email@domain.com>', false);
+    });
+    test('two @ signs', () {
+      testEmail('email@domain@domain.com', false);
+    });
+    test('unicode in address', () {
+      testEmail('?????@domain.com', false);
+    });
+    test('text after address', () {
+      testEmail('email@domain.com (Joe Smith)', false);
+    });
+    test('multiple dots in domain', () {
+      testEmail('email@domain..com', false);
+    });
+  });
 }
--- test/gold_zip_input_test.dart
+++ test/gold_zip_input_test.dart
@@ -8,82 +8,87 @@ import 'package:test/test.dart';
 import 'common.dart';
 
 main() async {
-    await initWebComponents();
+  await initWebComponents();
 
-    group('basic', () {
-        test('invalid input is not ok', () {
-            GoldZipInput input = fixture('basic');
-            input.value='1234';
-            forceXIfStamp(input);
+  group('basic', () {
+    test('invalid input is not ok', () {
+      GoldZipInput input = fixture('basic');
+      input.value = '1234';
+      forceXIfStamp(input);
 
-            var container = input.querySelector('paper-input-container');
-            expect(container, isNotNull);
-            expect(container.invalid, isTrue);
-        });
-
-        test('input is formatted ok', () {
-            GoldZipInput input = fixture('basic');
-            input.value='902101111';
-            expect(input.value, equals('90210-1111'));
-        });
-
-        test('spaces instead of dashes are not ok', () {
-            GoldZipInput input = fixture('basic');
-            input.value='90210 1111';
-            forceXIfStamp(input);
-
-            var container = input.querySelector('paper-input-container');
-            expect(container, isNotNull);
-            expect(container.invalid, isTrue);
-        });
-
-        test('short zipcodes are ok', () {
-            GoldZipInput input = fixture('basic');
-            input.value='94109';
-            forceXIfStamp(input);
-
-            var container = input.querySelector('paper-input-container');
-            expect(container, isNotNull);
-            expect(container.invalid, isFalse);
-        });
-
-        test('full zipcodes are ok', () {
-            GoldZipInput input = fixture('basic');
-            input.value='94109-1234';
-            forceXIfStamp(input);
-
-            var container = input.querySelector('paper-input-container');
-            expect(container, isNotNull);
-            expect(container.invalid, isFalse);
-        });
-
-        test('empty required input shows error', () {
-            GoldZipInput input = fixture('basic');
-            forceXIfStamp(input);
-
-            var error = input.querySelector('paper-input-error');
-            expect(error, isNotNull);
-            expect(error.getComputedStyle().display, isNot('none'), reason: 'error should not be display:none');
-        });
-
-        test('caret position is preserved', () {
-            GoldZipInput input = fixture('basic');
-            IronInput ironInput = input.querySelector('input[is="iron-input"]');
-            input.value='11111-11';
-            ironInput.selectionStart = 2;
-            ironInput.selectionEnd = 2;
-            input.jsElement.callMethod('_computeValue', ['11221-1111']);
-
-            expect(ironInput.selectionStart, equals(2), reason: 'selectionStart should be preserved');
-            expect(ironInput.selectionEnd, equals(2), reason: 'selectionEnd should be preserved');
-        });
+      var container = input.querySelector('paper-input-container');
+      expect(container, isNotNull);
+      expect(container.invalid, isTrue);
     });
 
-    group('a11y', () {
-        test('has aria-labelledby', () {
-            GoldZipInput input = fixture('basic');
-            expect(input.inputElement.getAttribute('aria-labelledby'), isNotNull);
-            expect(input.inputElement.getAttribute('aria-labelledby'), input.querySelector('label').id, reason: 'aria-labelledby points to the label');
-        });
+    test('input is formatted ok', () {
+      GoldZipInput input = fixture('basic');
+      input.value = '902101111';
+      expect(input.value, equals('90210-1111'));
     });
+
+    test('spaces instead of dashes are not ok', () {
+      GoldZipInput input = fixture('basic');
+      input.value = '90210 1111';
+      forceXIfStamp(input);
+
+      var container = input.querySelector('paper-input-container');
+      expect(container, isNotNull);
+      expect(container.invalid, isTrue);
+    });
+
+    test('short zipcodes are ok', () {
+      GoldZipInput input = fixture('basic');
+      input.value = '94109';
+      forceXIfStamp(input);
+
+      var container = input.querySelector('paper-input-container');
+      expect(container, isNotNull);
+      expect(container.invalid, isFalse);
+    });
+
+    test('full zipcodes are ok', () {
+      GoldZipInput input = fixture('basic');
+      input.value = '94109-1234';
+      forceXIfStamp(input);
+
+      var container = input.querySelector('paper-input-container');
+      expect(container, isNotNull);
+      expect(container.invalid, isFalse);
+    });
+
+    test('empty required input shows error', () {
+      GoldZipInput input = fixture('basic');
+      forceXIfStamp(input);
+
+      var error = input.querySelector('paper-input-error');
+      expect(error, isNotNull);
+      expect(error.getComputedStyle().display, isNot('none'),
+          reason: 'error should not be display:none');
+    });
+
+    test('caret position is preserved', () {
+      GoldZipInput input = fixture('basic');
+      IronInput ironInput = input.querySelector('input[is="iron-input"]');
+      input.value = '11111-11';
+      ironInput.selectionStart = 2;
+      ironInput.selectionEnd = 2;
+      input.jsElement.callMethod('_computeValue', ['11221-1111']);
+
+      expect(ironInput.selectionStart, equals(2),
+          reason: 'selectionStart should be preserved');
+      expect(ironInput.selectionEnd, equals(2),
+          reason: 'selectionEnd should be preserved');
+    });
+  });
+
+  group('a11y', () {
+    test('has aria-labelledby', () {
+      GoldZipInput input = fixture('basic');
+      expect(input.inputElement.getAttribute('aria-labelledby'), isNotNull);
+      expect(input.inputElement.getAttribute('aria-labelledby'),
+          input.querySelector('label').id,
+          reason: 'aria-labelledby points to the label');
+    });
+  });
 }
